a0ee52a5689beacdba71f2f03a97ce39842662f0
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-06-20 20:51:23 +0000

    Update and rename artix-install-openrc/artix-install.py to artix-install-runit/artix-install.py

--- artix-install-runit/artix-install.py
+++ artix-install-runit/artix-install.py
@@ -21,7 +21,7 @@ os.system("mkdir -p /mnt/boot/efi")
 os.system("mount " f"{boot_partition} " "/mnt/boot/efi")
 
 os.system("clear")
-os.system("basestrap -i /mnt base base-devel linux linux-headers linux-firmware openrc git nano")
+os.system("basestrap -i /mnt base base-devel linux linux-headers linux-firmware runit git nano")
 os.system("fstabgen -U /mnt >> /mnt/etc/fstab")
 os.system("cp $(pwd)/chroot.py /mnt")
 os.system("artix-chroot /mnt /bin/bash")
